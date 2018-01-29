337c9452850c06202a1224bc23dc4229c3f896a7
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-01-29 11:24:16 -0800

    Load config from python file

---
+++ demo/config-example.py
@@ -0,0 +1,6 @@
+class Config(object):
+    API_URL = 'https://api.example.com/v1/spendingauthority'
+    TOKEN_URL = 'https://api.oregonstate.edu/oauth2/token'
+    CLIENT_ID = 'client_id'
+    CLIENT_SECRET = 'client_secret'
+    SECRET_KEY = 'secret_key'  # this secret key is for client-side sessions security
