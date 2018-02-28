c673027c174f2ea97214481cf8e6439562dff325
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-02-27 19:32:43 -0700

    add logout link

--- demo/config-example.py
+++ demo/config-example.py
@@ -4,3 +4,6 @@ class Config(object):
     CLIENT_ID = 'client_id'
     CLIENT_SECRET = 'client_secret'
     SECRET_KEY = 'secret_key'  # this secret key is for CSRF prevention
+    CAS_URL = 'https://login.oregonstate.edu/cas-dev'
+    SERVICE_URL = 'http://localhost:5000/' # url used by cas to redirect after authenticating
+    AUTHORIZED_USERS = ['doejohn'] # list of users authorized to use the application
